ddbbdd5a47c240e46278f06bfd9fef8f6a3d35ed
noyon-360 <nazibullahnoyon19.20@gmail.com>
2026-09-17 14:11:47 +0600

    Remove debug logging from login method in AuthRepoImpl

--- lib/features/auth/repo/auth_repo.dart
+++ lib/features/auth/repo/auth_repo.dart
@@ -1,6 +1,5 @@
 import 'package:flutter_kaiyaletz/core/constants/api_constants.dart';
 import 'package:flutter_kaiyaletz/core/providers/core_provider.dart';
-import 'package:flutter_kaiyaletz/core/utils/d_print.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import '../../../core/api/api_client.dart';
 import '../../../core/models/network_result.dart';
@@ -30,17 +29,11 @@ class AuthRepoImpl implements AuthRepo {
     required String email,
     required String password,
   }) {
-    DPrint.log("Login Working...");
-
-    final data = apiClient.post<Map<String, dynamic>>(
+    return apiClient.post<Map<String, dynamic>>(
       endpoint: ApiConstants.auth.login,
       data: {'email': email, 'password': password},
       fromJsonT: (json) => json as Map<String, dynamic>,
     );
-
-    DPrint.log("Repo : ${data}");
-
-    return data;
   }
 
   @override
